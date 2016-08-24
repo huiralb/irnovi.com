5ef6e42d120787401a24e7d189f8f5102fc67a4e
huiralb <huiralb@gmail.com>
2016-08-24 13:45:40 +0700

    create tables : posts, tags, post_tag

---
+++ database/migrations/2016_08_24_060832_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('author_id')->unsigned();
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+            $table->timestamp('published_at');
+            
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
