ac13a6e49bb91d1fcd19f3b5a8a1847158b7b59e
huiralb <huiralb@gmail.com>
2016-08-25 09:56:46 +0700

    user factory model

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,5 @@
 <?php
-
+use Faker\Factory as Faker;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -11,9 +11,22 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\User::class, function ($faker) {
+
+    // override from Faker\Generator to Faker\Factory
+    // This is for Indonesia location
+    $faker = Faker::create('id_ID');
+
     return [
         'name' => $faker->name,
         'email' => $faker->email,
+        'password' => str_random(10),
+        'remember_token' => str_random(10),
     ];
 });
+
+/**
+ * call it to insert data to table
+ */
+ 
+ # $users = factory(App\User::class, 5)->create();
